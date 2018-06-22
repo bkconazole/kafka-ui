5222139690cfeadbb573c98d93d15de441974aa3
brook ambaw <brook.ambaw1@gmail.com>
2018-06-22 17:06:20 -0500

    side-bar nav implementation

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewChecked } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-sidebar',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidebar.component.scss']
 })
 export class SidebarComponent implements OnInit {
-
+ 
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $('#sidebarCollapse').on('click', function () {
+          $('#sidebar').toggleClass('active');
+          $(this).toggleClass('active');
+      });
+  });
   }
 
 }
