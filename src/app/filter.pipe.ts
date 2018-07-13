9571b309abec09c7b71707bccb2d512e1d6a6365
brook ambaw <brook.ambaw1@gmail.com>
2018-07-13 10:57:57 -0500

    chore: remove tests

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -6,11 +6,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class FilterPipe implements PipeTransform {
  
   transform(topics: any, term: any): any {
-    console.log("filter pipe is called");
     if(term === undefined) return topics;
     return topics.filter( function(topic){
       return topic.toLowerCase().includes(term.toLowerCase());
     })
-  }
+  }k
 
 }
