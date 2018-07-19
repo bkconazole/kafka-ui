109a3e97353009aa6285c1008a43c76e314b6087
brook ambaw <brook.ambaw1@gmail.com>
2018-07-19 17:35:01 -0500

    chore: file upload improvments

--- src/app/components/create-topic/create-topic.component.ts
+++ src/app/components/create-topic/create-topic.component.ts
@@ -3,6 +3,7 @@ import { DataService } from '../../services/data.service';
 import { ElementRef, ViewChild } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators, FormsModule } from "@angular/forms";
 import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-create-topic',
@@ -11,41 +12,79 @@ import { HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 })
 export class CreateTopicComponent implements OnInit {
   private fileText;
-  
-  constructor( private dataService: DataService ) { 
-    
+  res$: any;
+  error$: any;
+  private newText;
+
+  constructor(private dataService: DataService) {
+
   }
   ngOnInit() {
   }
 
   createTopic() {
-    console.log("create topic is called");
+    console.log("create topic is called with..." + this.newText);
     this.dataService.createTopic(this.fileText).subscribe(
       res => {
         console.log("http response starts here...");
-        console.log(res);
+        //console.log(res);
         console.log("http response ends here...");
       },
-      ( err: HttpErrorResponse ) => {
+      (err: HttpErrorResponse) => {
         console.log("error message starts here....");
-        console.log(err.error);
-        console.log(err.name);
-        console.log(err.message);
-        console.log(err.status);
+        console.log('error: ' + err.error);
+        console.log('name: ' + err.name);
+        console.log('message: ' + err.message);
+        console.log('status: ' + err.status);
         console.log("error message ends here....");
       }
     );
   }
 
   fileUpload(event) {
-    let reader = new FileReader();
-    reader.readAsText(event.srcElement.files[0]);
-    let me = this;
-    reader.onload = function () {
-      me.fileText = reader.result;
-
-      console.log('file text in crate topic'+me.fileText);
+    let fileName = event.target.files[0].name;
+    let fileExtenstion = fileName.substring(fileName.lastIndexOf('.') + 1);
+    /* original file upload function
+    fileUpload(event) {
+      let reader = new FileReader();
+      reader.readAsText(event.srcElement.files[0]);
+      let me = this;
+      reader.onload = function () {
+        me.fileText = reader.result;
+      }
+    }
+    */
+    if (fileExtenstion === 'json') {
+      let reader = new FileReader();
+      reader.readAsText(event.srcElement.files[0]);
+      let me = this;
+      reader.onload = function () {
+        me.fileText = reader.result;
+      }
+      return;
+    } else if (fileExtenstion === 'txt') {
+      let reader = new FileReader();
+      reader.readAsText(event.srcElement.files[0]);
+      let me = this;
+      reader.onload = function () {
+        let rd = new FileReader();
+        console.log('text file before split' + me.fileText);
+        var t = reader.result.split(';');
+        console.log('text file after split' + t);
+        me.newText = '{';
+        for (var i = 0; i < t.length; i++) {
+          let pair = t[i].split(':');
+          me.newText += `"${pair[0]}":"${pair[1]}"`
+          if (i !== t.length - 1) {
+            me.newText += ',\n';
+          }
+        }
+        me.newText += '}';
+        console.log("final form : " + me.newText);
+      }
+      return;
     }
   }
+
 
 }
